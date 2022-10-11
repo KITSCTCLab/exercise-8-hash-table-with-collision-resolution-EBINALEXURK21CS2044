2074e1db0cf333f5ee0eb2b7ef13e9426abbf359
Ebin Alex <ebinalex@karunya.edu.in>
2022-10-11 21:06:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import re
 
 def display_hash(hashtable) -> None:
-    for element in range(len(hashtable')):
+    for element in range(len(hashtable)):
         print(f"{element}", end=" ")
         values = hashtable[element]
         for value in values:
